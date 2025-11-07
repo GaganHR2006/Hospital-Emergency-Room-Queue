7e18eccab0616819088ac79e01746c03b4949387
Gagan H R <gagangagan3352nothing@gmail.com>
2025-11-07 23:30:16 +0530

    Feat: Define Patient struct and heap helper functions

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct {
+    int priority;
+    char name[20];
+}Patient;
+
+Patient heap[100];
+int heapSize = 0;
+
+void swap(Patient *a,Patient *b)
+{
+    Patient temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int parent(int i)
+{
+    return i/2;
+}
+int leftChild(int i)
+{
+    return 2*i;
+}
+int rightChild(int i)
+{
+    return (2*i)+1;
+}
 int main() {
     printf("Hello, ER! Program is running.\n");
     return 0;
